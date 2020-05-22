c1c4ac59c0737f9c460e57b2ae9b5f6f5ac3e863
gotomts <mh.goto.web@gmail.com>
2020-05-22 09:50:57 +0900

    テストファイルディレクトリ移動

--- src/tests/Unit/Models/ImageTest.php
+++ src/tests/Unit/Models/ImageTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Tests\Feature\Models;
+namespace Tests\Unit\Models;
 
 use App\Models\Image;
 use App\Models\User;
---
+++ src/tests/Unit/Models/PostTest.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Tests\Unit\Models;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class PostTest extends TestCase
+{
+    /** @test */
+    public function testGetByUrl()
+    {
+        $this->assertTrue(true);
+    }
+
+    /** @test */
+    public function testGetAll()
+    {
+        $this->assertTrue(true);
+    }
+
+    /** @test */
+    public function testGetPublishingAll()
+    {
+        $this->assertTrue(true);
+    }
+
+    /** @test */
+    public function testGetPageLinkUrl()
+    {
+        $this->assertTrue(true);
+    }
+
+    /** @test */
+    public function testGetPostCategoryAll()
+    {
+        $this->assertTrue(true);
+    }
+
+    /** @test */
+    public function testInsert()
+    {
+        $this->assertTrue(true);
+    }
+
+    /** @test */
+    public function testInsertWithPostImage()
+    {
+        $this->assertTrue(true);
+    }
+
+    /** @test */
+    public function testUpdateById()
+    {
+        $this->assertTrue(true);
+    }
+
+    /** @test */
+    public function testUpdateByIdWithPostImage()
+    {
+        $this->assertTrue(true);
+    }
+}
