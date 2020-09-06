c4c35cf6bd16a0a69efeea6d18f76d27f04893a5
gotomts <mh.goto.web@gmail.com>
2020-09-07 08:44:42 +0900

    docker nginx-proxy用に調整

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-cd /var/www/test && git pull
+WORD_DIR=/var/www/devlog-mh
+
+cd ${WORD_DIR} && git pull
+docker exec devlog-mh_app_1 composer install
+docker exec devlog-mh_app_1 php artisan migrate
