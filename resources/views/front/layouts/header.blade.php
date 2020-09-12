c0dcc970ab8bd1390424ad8b1c611308a3f0de0d
gotomts <mh.goto.web@gmail.com>
2020-09-13 04:12:41 +0900

    deploy test

--- resources/views/front/layouts/header.blade.php
+++ resources/views/front/layouts/header.blade.php
@@ -1,6 +1,6 @@
 <header>
     <h1 class="logo text-center"><a href="/">{{ config('app.name') }}</a></h1>
-    <p class="text-center">Webデザイナーから転職してWeb系エンジニアとして働いています。日々の技術ログとして使いたい。</p>
+    <p class="text-center">Webデザイナーから転職してWeb系エンジニアとして働いています。日々の技術ログとして使いたい。test</p>
     <nav class="navbar navbar-expand-lg border-top border-bottom navbar-light nav-text-black mb-5">
         <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
         <div class="collapse navbar-collapse justify-content-md-center" id="navbar">
