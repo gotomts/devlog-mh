30c128e25a42adc1a1c01c958981c1fbf04596b8
gotomts <mh.goto.web@gmail.com>
2021-01-13 00:32:37 +0900

    fix(header.blade.php): ロゴのリンク不備修正

--- resources/views/front/layouts/header.blade.php
+++ resources/views/front/layouts/header.blade.php
@@ -3,7 +3,7 @@
 <header class="blog-header py-3">
     <div class="row flex-nowrap justify-content-between align-items-center">
         <div class="col-7">
-            <a class="blog-header-logo text-dark" href="index.html">Devlog</a>
+            <a class="blog-header-logo text-dark" href="/">Devlog</a>
         </div>
         <div class="col-5 d-flex justify-content-end align-items-center">
             @guest('member')
