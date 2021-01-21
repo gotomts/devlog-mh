628677dc3d28a70afce51e007b1c06a7d307d3c5
Mitsuharu Goto <mh.goto.web@gmail.com>
2021-01-22 00:42:28 +0900

    refactor: members_member_typesテーブルの構造変更
    members_idとmember_types_idで複合PKになるよう修正

--- database/migrations/2021_01_17_205605_create_members_member_types.php
+++ database/migrations/2021_01_17_205605_create_members_member_types.php
@@ -17,12 +17,10 @@ class CreateMembersMemberTypes extends Migration
     {
         if (!Schema::hasTable($this->tableName)) {
             Schema::create($this->tableName, function (Blueprint $table) {
-                $table->bigIncrements('id');
                 $table->unsignedBigInteger('members_id');
                 $table->unsignedBigInteger('member_types_id');
 
-                $table->foreign('members_id')->references('id')->on('members');
-                $table->foreign('member_types_id')->references('id')->on('member_types');
+                $table->primary(['members_id', 'member_types_id']);
             });
         }
     }
