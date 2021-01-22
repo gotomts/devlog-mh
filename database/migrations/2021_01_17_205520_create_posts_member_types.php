c36b3c34948ebb8d92cc31bad26fa4d0f726a7a3
Mitsuharu Goto <mh.goto.web@gmail.com>
2021-01-22 23:36:01 +0900

    refactor: posts_member_typesテーブルの構造を変更
    idの削除、posts_idとmember_types_idの複合PKに変更

--- database/migrations/2021_01_17_205520_create_posts_member_types.php
+++ database/migrations/2021_01_17_205520_create_posts_member_types.php
@@ -17,12 +17,10 @@ class CreatePostsMemberTypes extends Migration
     {
         if (!Schema::hasTable($this->tableName)) {
             Schema::create($this->tableName, function (Blueprint $table) {
-                $table->bigIncrements('id');
                 $table->unsignedBigInteger('posts_id');
                 $table->unsignedBigInteger('member_types_id');
 
-                $table->foreign('posts_id')->references('id')->on('posts');
-                $table->foreign('member_types_id')->references('id')->on('member_types');
+                $table->primary(['posts_id', 'member_types_id']);
             });
         }
     }
